b3e2b502867b94cf483ac7bb80db35d98eb54531
Web development <surjithbaigr01@gmail.com>
2025-04-21 13:14:19 +0530

    projects for javascript

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log("Hello, World!");
+console.log("This is a test script.");
+
+let number = 5;
+
+console.log("The number is: " + number); 
+
+
+
+
+var factor ='surjith';
+var fplayer = 'surjiths';
+var fmovie = 'surjithm';
+
+function favourite() {
+    console.log(factor);
+    console.log(fplayer);
+    console.log(fmovie);
+}
+
+favourite();
+
+function area(a, b) {
+    console.log (a*b);
+}
+
+area(5, 10);
+var num=10
+if (num < 5){
+    console.log("Number is less than 5");
+} else if (num == 5) {
+    console.log("Number is equal to 5");
+} else {
+    console.log("Number is greater than 5");
+}
+
+
+// for (var i=0; i<1000; i++){
+//     console.log(i)
+// }
+
+
+for( var num=1; num<10; num= num+2) {
+    console.log(num)
+}
+
+const a = (a, b) => {
+    console.log(a*b);
+}
+
+a(5, 10);
